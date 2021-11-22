fe187b64812a1c60828d24ecd50485100d06f86d
Lodenn <khalednasser788@gmail.com>
2021-11-22 21:27:55 +0200

    add global product styles to the rest of product data

--- src/components/Product/ProductInfo/ProductPrice/ProductPrice.jsx
+++ src/components/Product/ProductInfo/ProductPrice/ProductPrice.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import classes from "./ProductPrice.module.scss";
+import globalProductClasses from "../../GlobalProductStyles.module.scss";
 
 class ProductPrice extends React.Component {
   render() {
     return (
       <div className={`${classes["product-price"]} flex-y-container`}>
-        <p className={classes["product-price__main"]}>
+        <p className={globalProductClasses["product-info__price"]}>
           9,999{" "}
-          <span className={classes["product-price__main--currency"]}>L.E</span>
+          <span
+            className={globalProductClasses["product-info__price--currency"]}
+          >
+            L.E
+          </span>
         </p>
-        <p className={classes["product-price__old"]}>
+        <p className={globalProductClasses["product-info__old-price"]}>
           <del>9,999 L.E</del>
         </p>
-        <p className={classes["product-price__discount"]}>30% Off</p>
+        <p className={globalProductClasses["product-info__discount"]}>
+          30% Off
+        </p>
       </div>
     );
   }
