d8ca264fc79eef83cf639bef1ace989085f96103
Lodenn <khalednasser788@gmail.com>
2021-11-22 21:21:42 +0200

    add global product styles to product rate component

--- src/components/Product/ProductInfo/ProductRate/ProductRate.jsx
+++ src/components/Product/ProductInfo/ProductRate/ProductRate.jsx
@@ -2,41 +2,41 @@ import React from "react";
 import classes from "./ProductRate.module.scss";
 import { ReactComponent as StarIcon } from "../../../../images/icon-star.svg";
 import { ReactComponent as StarFillIcon } from "../../../../images/icon-star-fill.svg";
-
+import globalProductClasses from "../../GlobalProductStyles.module.scss";
 class ProductRate extends React.Component {
   render() {
     return (
       <div className={`${classes["product-rate"]} flex-y-container`}>
-        <ul className={classes["product-rate__stars"]}>
-          <li className={classes["product-rate__stars--star"]}>
+        <ul className={globalProductClasses["product-info__stars"]}>
+          <li className={globalProductClasses["product-info__stars--star"]}>
             <span className="icon">
               <StarFillIcon />
             </span>
           </li>
-          <li className={classes["product-rate__stars--star"]}>
+          <li className={globalProductClasses["product-info__stars--star"]}>
             <span className="icon">
               <StarFillIcon />
             </span>
           </li>
-          <li className={classes["product-rate__stars--star"]}>
+          <li className={globalProductClasses["product-info__stars--star"]}>
             <span className="icon">
               <StarFillIcon />
             </span>
           </li>
-          <li className={classes["product-rate__stars--star"]}>
+          <li className={globalProductClasses["product-info__stars--star"]}>
             <span className="icon">
               <StarFillIcon />
             </span>
           </li>
           <li
-            className={`${classes["product-rate__stars--star"]} ${classes["product-rate__stars--star-active"]}`}
+            className={`${globalProductClasses["product-info__stars--star"]} ${globalProductClasses["product-info__stars--star-active"]}`}
           >
             <span className="icon">
               <StarIcon />
             </span>
           </li>
         </ul>
-        <div className={classes["product-rate__rate-value"]}>
+        <div className={globalProductClasses["product-info__rate-value"]}>
           <span className={classes["product-rate__rate-value--val"]}>4.9</span>{" "}
           of 5
         </div>
